5ce233cf7d3d13f9e98537e8f48523bc41605013
Wolf at the Doors <kontakt@gisela-wolf.de>
2023-01-25 13:19:58 +0100

    Userproofing IPv6

--- src/IPv4Calculator.java
+++ src/IPv4Calculator.java
@@ -527,28 +527,21 @@ class IPv4 {
 
 class IPv6 {
     //IP Adress
-    private static int[] IPv6Hex = {0xabcd, 0xabcd, 0xabcd, 0xabcd, 0xabcd, 0xabcd, 0xabcd, 0xabcd};
+    private static String[] IPv6Hex = {"abcd", "abcd", "abcd", "abcd", "abcd", "abcd", "abcd", "abcd"};
     static String[] IPv6BinString = {"0000", "0000", "0000", "0000", "0000", "0000", "0000", "0000"};
     static int[] IPv6BinInt = {0, 0, 0, 0, 0, 0, 0, 0};
     static boolean input = true;
-    public static int getIPv6Hex(int index) {
+    public static String getIPv6Hex(int index) {
         return IPv6Hex[index];
     }
-    public static void setIPv6Hex(int IPv6Hex, int index) {
+    public static void setIPv6Hex(String IPv6Hex, int index) {
 
-            if (IPv6Hex > 0 && IPv6Hex < 0xffff) {
-                /*
-                if (IPv6Hex.length() < 4) {
-                    setIPv6Hex(String.format("%04x", IPv6Hex), i);      //padding
-                }*/
-
-                IPv6.IPv6Hex[index] = IPv6Hex;
-                input = true;
-            } else {
-                input = false;
-                System.out.println("That is not a valid octet. Please try again");
-            }
+        if (String.valueOf(IPv6Hex).length() < 4) {
+            IPv6.IPv6Hex[index] = String.format("%04x", IPv6Hex);
+        } else {
+            IPv6.IPv6Hex[index] = String.valueOf(IPv6Hex);
         }
+    }
 
     //Subnetmask
     private static int subnetSlash = 0;
@@ -579,14 +572,14 @@ class IPv6 {
         System.out.println("First the IP-Adress.");
 
         for(int i = 0; i<8; i++) {
-            System.out.println("Please enter the " +i+1 +". octet:");
-            setIPv6Hex(in.nextInt(), i);
+            System.out.println("Please enter the " +(i+1) +". octet:");
+            setIPv6Hex(in.nextLine(), i);
         }
 
 //converts to binary
-     //   for (int i = 0; i < 8; i++) {
-     //       IPv6BinString[i] = hex.hexToBin(getIPv6Hex(i)));
-     //   }
+        for (int i = 0; i < 8; i++) {
+            IPv6BinString[i] = hex.hexToBin(getIPv6Hex(i));
+        }
 
 //input Subnetmask
         System.out.println("And now the subnetmask please.");
